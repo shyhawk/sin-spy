9e980b037d1eade4f1e3b3139e06b6a2fe9e2a2c
shyhawk <grapesareyum@gmail.com>
2016-12-29 22:47:48 -0600

    Add basic finding/adding of characters and players

--- db.js
+++ db.js
@@ -31,15 +31,116 @@ module.exports = function(dbString, logging, callback) {
 			return collectionForType;
 		}
 
-		var colTypePlayer = publicDb.colTypePlayer = function () { return "player"; };
+		var colTypePlayer = publicDb.colTypePlayer = function() { return "player"; };
 		getCollection(colTypePlayer());
-
 		
-		var colTypeCharacter = publicDb.colTypeCharacter = function () { return "character"; };
+		var colTypeCharacter = publicDb.colTypeCharacter = function() { return "character"; };
 		getCollection("character");
-
 		
 		var colTypeUser = publicDb.colTypeUser = function() { return "user"; };
 		getCollection("user");
+
+		// Queues and Queue Functions
+		var documentQueueSet = {}; // allows queueing up objects for more efficient batch database operations
+
+		publicDb.queuePlayer = function(id, name) {
+			var playerData = {
+	            id: id,
+	            name: name,
+	            characters: [],
+	            logs: []
+	        };
+			return queueObject(colTypePlayer(), playerData);
+		};
+
+		publicDb.queueCharacter = function(id, player) {
+			var characterData = {
+	            id: id,
+	            player: player,
+	            logs: []
+	        };
+			return queueObject(colTypeCharacter(), characterData);
+		};
+
+		function queueObject(type, data) {
+			var queueSet = documentQueueSet[type];
+			if (!queueSet) 
+				queueSet = documentQueueSet[type] = {};
+
+			queueSet[data.id] = data;
+			return data;
+		}
+
+		publicDb.findOrAddQueued = function(colType, findCallback, addCallback, completeCallback) {
+			findQueued(colType, findCallback, function() {
+				console.log("Finding done! Time to add...");
+				addQueued(colType, addCallback, completeCallback);
+			});
+		};
+
+		function findQueued(colType, findCallback, completeCallback) {
+			var queueSet = documentQueueSet[colType];
+			var idList = Object.keys(queueSet);
+
+			if (idList.length === 0) {
+				completeCallback();
+				return;
+			}
+
+			var collection = getCollection(colType);
+			var cursor = collection.find({"id": {$in: idList}});
+
+			cursor.count(function (err, count) {
+				if (err) {
+					throw(err);
+				}
+
+				if (count === 0) {
+					completeCallback();
+					return;
+				}
+
+				var counted = 0;
+				cursor.forEach(function(doc, index) {
+					counted++;
+					delete queueSet[doc.id];
+					findCallback(doc);
+
+					if (counted === count){
+						completeCallback(); // only fire on final item
+					}
+				});
+			});
+		}
+
+		function addQueued(colType, addCallback, completeCallback) {
+			var queueSet = documentQueueSet[colType];
+			var queued = Object.keys(queueSet).map(function(id, index) {
+				return queueSet[id];
+			});
+
+			if (queued.length === 0) {
+				completeCallback(); // if nothing to insert, just call complete and be done
+				return;
+			}
+
+			var collection = getCollection(colType);
+			collection.insert(queued, {w:1}, function(err, result) {
+				if (err) {
+					throw(err);
+				}
+
+				var counted = 0;
+				queued.forEach(function(doc, index) {
+					counted++
+					delete queueSet[doc.id]; // clear from set
+					addCallback(doc);
+
+					if (counted === queued.length) {
+						completeCallback(); // only fire on final item
+					}
+				});
+			});
+		}
 	}
 }
